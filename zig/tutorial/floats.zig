aebbfe4e88673110b944386cf0d9b9e965c4d57f
neoki <ot07neoki@gmail.com>
2024-06-04 21:53:44 +0900

    zig/tutorial: add floats code

---
+++ zig/tutorial/floats.zig
@@ -0,0 +1,15 @@
+const expect = @import("std").testing.expect;
+
+test "float widening" {
+    const a: f16 = 0;
+    const b: f32 = a;
+    const c: f128 = b;
+    try expect(c == @as(f128, a));
+}
+
+test "int-float conversion" {
+    const a: i32 = 0;
+    const b = @as(f32, @floatFromInt(a));
+    const c = @as(i32, @intFromFloat(b));
+    try expect(c == a);
+}
