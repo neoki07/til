6b44bb0b2d8134cd8ec39559c5cf21f285f835f9
neoki <ot07neoki@gmail.com>
2024-06-04 21:38:52 +0900

    zig/tutorial: add integer-rules code

---
+++ zig/tutorial/integer-rules.zig
@@ -0,0 +1,20 @@
+const expect = @import("std").testing.expect;
+
+test "integer widening" {
+    const a: u8 = 250;
+    const b: u16 = a;
+    const c: u32 = b;
+    try expect(c == a);
+}
+
+test "@intCast" {
+    const x: u64 = 200;
+    const y = @as(u8, @intCast(x));
+    try expect(@TypeOf(y) == u8);
+}
+
+test "well defined overflow" {
+    var a: u8 = 255;
+    a +%= 1;
+    try expect(a == 0);
+}
